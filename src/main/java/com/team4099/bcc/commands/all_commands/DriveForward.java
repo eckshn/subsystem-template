97b406cab725dbb9dac825602349756a19a522fb
Eckart Schneider <eckshn@gmail.com>
2021-05-20 20:49:55 -0400

    switch to workshop 12

---
+++ src/main/java/com/team4099/bcc/commands/all_commands/DriveForward.java
@@ -0,0 +1,6 @@
+package com.team4099.bcc.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DriveForwardCommand extends CommandBase {
+}
--- src/main/java/com/team4099/bcc/subsystems/Shooter.java
+++
@@ -1,7 +0,0 @@
-package com.team4099.bcc.subsystems;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Shooter extends SubsystemBase {
-
-}
